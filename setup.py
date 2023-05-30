cea8d4a174f4c81a850e91fe71c78e121d012498
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2023-05-30 11:44:26 +0800

    alvistack/1.7.0
        git clean -xdf
        tar zcvf ../python-poetry-core_1.7.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-poetry-core.spec ../python-poetry-core_1.7.0-1.spec
        mv ../python*-poetry-core*1.7.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/python-poetry-poetry-core-1.7.0/
        rm -rf ../python*-poetry-core*1.7.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['poetry',
+ 'poetry.core',
+ 'poetry.core._vendor',
+ 'poetry.core._vendor.attr',
+ 'poetry.core._vendor.attrs',
+ 'poetry.core._vendor.jsonschema',
+ 'poetry.core._vendor.jsonschema.benchmarks',
+ 'poetry.core._vendor.lark',
+ 'poetry.core._vendor.lark.__pyinstaller',
+ 'poetry.core._vendor.lark.grammars',
+ 'poetry.core._vendor.lark.parsers',
+ 'poetry.core._vendor.lark.tools',
+ 'poetry.core._vendor.packaging',
+ 'poetry.core._vendor.pyrsistent',
+ 'poetry.core._vendor.tomli',
+ 'poetry.core.constraints',
+ 'poetry.core.constraints.generic',
+ 'poetry.core.constraints.version',
+ 'poetry.core.exceptions',
+ 'poetry.core.json',
+ 'poetry.core.masonry',
+ 'poetry.core.masonry.builders',
+ 'poetry.core.masonry.utils',
+ 'poetry.core.packages',
+ 'poetry.core.packages.utils',
+ 'poetry.core.pyproject',
+ 'poetry.core.spdx',
+ 'poetry.core.utils',
+ 'poetry.core.vcs',
+ 'poetry.core.version',
+ 'poetry.core.version.grammars',
+ 'poetry.core.version.pep440']
+
+package_data = \
+{'': ['*'],
+ 'poetry.core._vendor.jsonschema': ['schemas/*',
+                                    'schemas/vocabularies/draft2019-09/*',
+                                    'schemas/vocabularies/draft2020-12/*'],
+ 'poetry.core._vendor.jsonschema.benchmarks': ['issue232/*'],
+ 'poetry.core.json': ['schemas/*'],
+ 'poetry.core.spdx': ['data/*']}
+
+setup_kwargs = {
+    'name': 'poetry-core',
+    'version': '1.7.0',
+    'description': 'Poetry PEP 517 Build Backend',
+    'author': 'Sébastien Eustace',
+    'author_email': 'sebastien@eustace.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/python-poetry/poetry-core',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
